8b77c1df1fbefce34f77ca67695be6b77395b160
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2023-11-29 22:08:30 +0300

    Initial commit Added mantine

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { MantineProvider, createTheme } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
 function App() {
   return (
+      <MantineProvider theme={theme}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +27,7 @@ function App() {
         </a>
       </header>
     </div>
+      </MantineProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+// core styles are required for all packages
+import '@mantine/core/styles.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
