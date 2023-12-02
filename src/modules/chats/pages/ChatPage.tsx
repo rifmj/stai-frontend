3b0df54a92627fe4c651f8671f3fb1798a5375cf
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2023-12-02 15:27:31 +0300

    Генерация функций из документации

--- src/modules/chats/pages/ChatPage.tsx
+++ src/modules/chats/pages/ChatPage.tsx
@@ -33,6 +33,8 @@ import {
 } from "@tabler/icons-react";
 import { observer } from "mobx-react";
 import React from "react";
+import { JSONTree } from "react-json-tree";
+import createStylingFromTheme from "react-json-tree/lib/types/createStylingFromTheme";
 import { useParams } from "react-router-dom";
 
 const ChatMessageTrace = (properties: {
@@ -78,11 +80,28 @@ const ChatMessageTrace = (properties: {
               </Group>
             </Group>
             {/*@ts-ignore*/}
-            <JsonInput
-              autosize
-              maxRows={12}
-              minRows={3}
-              value={JSON.stringify(value.attributes, null, 2)}
+            <JSONTree
+              theme={{
+                author: "wimer hazenberg (http://www.monokai.nl)",
+                base00: "#272822",
+                base01: "#383830",
+                base02: "#49483e",
+                base03: "#75715e",
+                base04: "#a59f85",
+                base05: "#f8f8f2",
+                base06: "#f5f4f1",
+                base07: "#f9f8f5",
+                base08: "#f92672",
+                base09: "#fd971f",
+                base0A: "#f4bf75",
+                base0B: "#a6e22e",
+                base0C: "#a1efe4",
+                base0D: "#66d9ef",
+                base0E: "#ae81ff",
+                base0F: "#cc6633",
+                scheme: "monokai",
+              }}
+              data={value.attributes}
             />
             <Stack gap={4}>
               {value.attributes.duration ? (
