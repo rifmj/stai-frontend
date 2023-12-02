06c34734b9b2d2e38e02c1b0f959404ca5ab9854
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2023-12-03 00:34:25 +0300

    Отображение function calls в сообщениях

--- src/modules/chats/components/ChatMessageTrace.tsx
+++ src/modules/chats/components/ChatMessageTrace.tsx
@@ -1,8 +1,39 @@
 import { useMobXStore } from "@/core/store/useMobXStore";
 import { useChatMessageTrace } from "@/modules/chats/hooks";
-import { Badge, Group, Paper, Stack, Text } from "@mantine/core";
-import React from "react";
+import {
+  Badge,
+  Divider,
+  Group,
+  Mark,
+  Paper,
+  Stack,
+  Text,
+  Tooltip,
+} from "@mantine/core";
+import {
+  IconClockFilled,
+  IconPentagon,
+  IconPentagonFilled,
+  IconTimeDuration0,
+  IconTimeline,
+} from "@tabler/icons-react";
+import React, { useCallback } from "react";
 import { JSONTree } from "react-json-tree";
+
+const filterKeys = (value) => !["duration", "end", "start"].includes(value);
+
+const getMarkColor = (duration: number) => {
+  if (duration >= 2000) {
+    return "red";
+  }
+  if (duration >= 1500) {
+    return "orange";
+  }
+  if (duration >= 1000) {
+    return "yellow";
+  }
+  return "green";
+};
 
 export const ChatMessageTrace = (properties: {
   chatId: string;
@@ -14,9 +45,73 @@ export const ChatMessageTrace = (properties: {
     properties.chatId,
     properties.messageId,
   );
+  const spans = trace.list.flatMap((value, index, array) => {
+    const tags = Object.keys(value.attributes).filter((value) =>
+      filterKeys(value),
+    );
+    if (tags.length === 0) return [];
+    const tag = tags[0];
+    const previousStart = array[index - 1] ?? array[0];
+    return [
+      {
+        data: value.attributes,
+        delta: value.attributes.start - previousStart.attributes.start,
+        duration: value.attributes.duration,
+        end: value.attributes.end,
+        id: value.span_id,
+        start: value.attributes.start,
+        type: tag,
+      },
+    ];
+  });
+
+  const renderRow = (key: string, value: string) => (
+    <Text size={"xs"}>
+      <strong>{key}: </strong>
+      {value}
+    </Text>
+  );
+
+  const renderRowIfExists = (key: string, value: string) => {
+    return value ? renderRow(key, value) : null;
+  };
+
   return (
     <Stack>
-      <Stack gap={0}></Stack>
+      <Stack gap={0}>
+        {spans.map((value) => (
+          <Stack gap={4}>
+            <Badge color={"grape"} size="xs" variant="light">
+              {value.type}
+            </Badge>
+            {renderRowIfExists(`Project Id`, value.data.project?.project_id)}
+            {renderRowIfExists(`Channel Id`, value.data.channel?.channel_id)}
+            {renderRowIfExists(`Chat Id`, value.data.message?.chat_id)}
+            {renderRowIfExists(`Client Id`, value.data.message?.client_id)}
+            {renderRowIfExists(`Message Id`, value.data.message?.message_id)}
+            {renderRowIfExists(`Request`, value.data.database?.request)}
+            {renderRowIfExists(`Request`, value.data.database?.result?.request)}
+            {renderRowIfExists(`Request`, value.data.mixed?.request)}
+            {renderRowIfExists(`Type`, value.data.openai?.type)}
+            {renderRowIfExists(`Action`, value.data.channel?.action)}
+            {value.duration ? (
+              <Tooltip label="Duration" offset={5} position="bottom">
+                <Group gap={4}>
+                  <IconClockFilled size={12} />
+                  <Text size={"xs"}>
+                    {Number.parseFloat(value.duration).toFixed(2)} ms
+                  </Text>
+                </Group>
+              </Tooltip>
+            ) : null}
+            <Divider
+              label={value.delta ? `+ ${value.delta?.toFixed(2)} ms` : null}
+              mb={8}
+              mt={4}
+            />
+          </Stack>
+        ))}
+      </Stack>
       {trace.list.map((value, index, array) => (
         <Paper>
           <Stack gap={"md"}>
@@ -37,9 +132,7 @@ export const ChatMessageTrace = (properties: {
               </Stack>
               <Group gap={"xs"}>
                 {Object.keys(value.attributes)
-                  .filter(
-                    (value) => !["duration", "end", "start"].includes(value),
-                  )
+                  .filter((value) => filterKeys(value))
                   .map((value) => (
                     <Badge color={"grape"} size="xs" variant="light">
                       {value}
