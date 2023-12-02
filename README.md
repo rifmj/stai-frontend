56a58a326be8c1b445e55527ccdd1d285c32735e
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2023-12-03 01:10:04 +0300

    Добавлена echarts для визуализации трейсов

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ https://nivo.rocks/swarmplot/
 https://echarts.apache.org/examples/en/editor.html?c=bar-waterfall2 !
 https://airbnb.io/visx/gallery
 https://react-chartjs-2.js.org/examples/scatter-chart
+https://recharts.org/en-US/examples/PieResponsiveContainer
+https://formidable.com/open-source/victory/gallery/
+
+https://ant-design-charts.antgroup.com/en/examples/tiny/tiny-column#basic-column !!
+https://ant-design-charts.antgroup.com/en/examples/more-plots/waterfall#basic !!
--- src/modules/chats/components/ChatMessageTrace.tsx
+++ src/modules/chats/components/ChatMessageTrace.tsx
@@ -2,11 +2,13 @@ import { useMobXStore } from "@/core/store/useMobXStore";
 import { useChatMessageTrace } from "@/modules/chats/hooks";
 import {
   Badge,
+  Box,
   Divider,
   Group,
   Mark,
   Paper,
   Stack,
+  Tabs,
   Text,
   Tooltip,
 } from "@mantine/core";
@@ -17,8 +19,83 @@ import {
   IconTimeDuration0,
   IconTimeline,
 } from "@tabler/icons-react";
+// Import charts, all with Chart suffix
+import {
+  // LineChart,
+  BarChart,
+  // PieChart,
+  // ScatterChart,
+  // RadarChart,
+  // MapChart,
+  // TreeChart,
+  // TreemapChart,
+  // GraphChart,
+  // GaugeChart,
+  // FunnelChart,
+  // ParallelChart,
+  // SankeyChart,
+  // BoxplotChart,
+  // CandlestickChart,
+  // EffectScatterChart,
+  // LinesChart,
+  // HeatmapChart,
+  // PictorialBarChart,
+  // ThemeRiverChart,
+  // SunburstChart,
+  // CustomChart,
+} from "echarts/charts";
+import {
+  // TransformComponent,
+  DatasetComponent,
+  // PolarComponent,
+  // RadarComponent,
+  // GeoComponent,
+  // SingleAxisComponent,
+  // ParallelComponent,
+  // CalendarComponent,
+  // GraphicComponent,
+  // GridSimpleComponent,
+  GridComponent,
+  // AxisPointerComponent,
+  // BrushComponent,
+  TitleComponent,
+  // TimelineComponent,
+  // MarkPointComponent,
+  // MarkLineComponent,
+  // MarkAreaComponent,
+  // LegendComponent,
+  // LegendScrollComponent,
+  // LegendPlainComponent,
+  // DataZoomComponent,
+  // DataZoomInsideComponent,
+  // DataZoomSliderComponent,
+  // VisualMapComponent,
+  // VisualMapContinuousComponent,
+  // VisualMapPiecewiseComponent,
+  // AriaComponent,
+  // ToolboxComponent,
+  TooltipComponent,
+} from "echarts/components";
+// Import the echarts core module, which provides the necessary interfaces for using echarts.
+import * as echarts from "echarts/core";
+// Import renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
+import {
+  CanvasRenderer,
+  // SVGRenderer,
+} from "echarts/renderers";
+// import the core library.
+import ReactEChartsCore from "echarts-for-react/lib/core";
 import React, { useCallback } from "react";
 import { JSONTree } from "react-json-tree";
+
+// Register the required components
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  BarChart,
+  CanvasRenderer,
+]);
 
 const filterKeys = (value) => !["duration", "end", "start"].includes(value);
 
@@ -76,105 +153,166 @@ export const ChatMessageTrace = (properties: {
     return value ? renderRow(key, value) : null;
   };
 
+  const spansWithDuration = spans.filter((value) => value.duration > 5);
+
   return (
     <Stack>
-      <Stack gap={0}>
-        {spans.map((value) => (
-          <Stack gap={4}>
-            <Badge color={"grape"} size="xs" variant="light">
-              {value.type}
-            </Badge>
-            {renderRowIfExists(`Project Id`, value.data.project?.project_id)}
-            {renderRowIfExists(`Channel Id`, value.data.channel?.channel_id)}
-            {renderRowIfExists(`Chat Id`, value.data.message?.chat_id)}
-            {renderRowIfExists(`Client Id`, value.data.message?.client_id)}
-            {renderRowIfExists(`Message Id`, value.data.message?.message_id)}
-            {renderRowIfExists(`Request`, value.data.database?.request)}
-            {renderRowIfExists(`Request`, value.data.database?.result?.request)}
-            {renderRowIfExists(`Request`, value.data.mixed?.request)}
-            {renderRowIfExists(`Type`, value.data.openai?.type)}
-            {renderRowIfExists(`Action`, value.data.channel?.action)}
-            {value.duration ? (
-              <Tooltip label="Duration" offset={5} position="bottom">
-                <Group gap={4}>
-                  <IconClockFilled size={12} />
-                  <Text size={"xs"}>
-                    {Number.parseFloat(value.duration).toFixed(2)} ms
-                  </Text>
-                </Group>
-              </Tooltip>
-            ) : null}
-            <Divider
-              label={value.delta ? `+ ${value.delta?.toFixed(2)} ms` : null}
-              mb={8}
-              mt={4}
-            />
-          </Stack>
-        ))}
-      </Stack>
-      {trace.list.map((value, index, array) => (
-        <Paper>
-          <Stack gap={"md"}>
-            <Group justify={"space-between"}>
-              <Stack>
-                {value.attributes.start &&
-                array[index - 1]?.attributes?.start ? (
-                  <Text c={"dimmed"} size={"xs"}>
-                    <strong>+</strong>{" "}
-                    {value.attributes.start -
-                      array[index - 1]?.attributes.start}{" "}
-                    ms
-                  </Text>
-                ) : null}
-                <Text c={"dimmed"} size={"xs"}>
-                  <strong>ID</strong>: {value.span_id}
-                </Text>
-              </Stack>
-              <Group gap={"xs"}>
-                {Object.keys(value.attributes)
-                  .filter((value) => filterKeys(value))
-                  .map((value) => (
-                    <Badge color={"grape"} size="xs" variant="light">
-                      {value}
-                    </Badge>
-                  ))}
-              </Group>
-            </Group>
-            {/*@ts-ignore*/}
-            <JSONTree
-              theme={{
-                author: "wimer hazenberg (http://www.monokai.nl)",
-                base00: "#272822",
-                base01: "#383830",
-                base02: "#49483e",
-                base03: "#75715e",
-                base04: "#a59f85",
-                base05: "#f8f8f2",
-                base06: "#f5f4f1",
-                base07: "#f9f8f5",
-                base08: "#f92672",
-                base09: "#fd971f",
-                base0A: "#f4bf75",
-                base0B: "#a6e22e",
-                base0C: "#a1efe4",
-                base0D: "#66d9ef",
-                base0E: "#ae81ff",
-                base0F: "#cc6633",
-                scheme: "monokai",
+      <Tabs defaultValue="overall">
+        <Tabs.List>
+          <Tabs.Tab value="overall">Overall</Tabs.Tab>
+          <Tabs.Tab value="raw">Raw</Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel pt={12} value="overall">
+          <Box>
+            <ReactEChartsCore
+              option={{
+                grid: { left: "33%" },
+                series: [
+                  {
+                    data: spansWithDuration.map((value) =>
+                      value.duration.toFixed(2),
+                    ),
+                    type: "bar",
+                  },
+                ],
+                tooltip: {
+                  axisPointer: {
+                    type: "shadow",
+                  },
+                  trigger: "axis",
+                  valueFormatter: (value) => `${value} ms`,
+                },
+                xAxis: {
+                  type: "value",
+                },
+                yAxis: {
+                  data: spansWithDuration.map(
+                    (value) => value.data?.mixed?.request ?? value.type,
+                  ),
+                  type: "category",
+                },
               }}
-              data={value.attributes}
+              echarts={echarts}
+              lazyUpdate={true}
+              notMerge={true}
             />
-            <Stack gap={4}>
-              {value.attributes.duration ? (
-                <Text c={"dimmed"} size={"xs"}>
-                  <strong>Duration</strong>:{" "}
-                  {Number.parseFloat(value.attributes.duration)} ms
-                </Text>
-              ) : null}
-            </Stack>
+          </Box>
+
+          <Stack gap={0}>
+            {spans.map((value) => (
+              <Stack gap={4}>
+                <Badge color={"grape"} size="xs" variant="light">
+                  {value.type}
+                </Badge>
+                {renderRowIfExists(
+                  `Project Id`,
+                  value.data.project?.project_id,
+                )}
+                {renderRowIfExists(
+                  `Channel Id`,
+                  value.data.channel?.channel_id,
+                )}
+                {renderRowIfExists(`Chat Id`, value.data.message?.chat_id)}
+                {renderRowIfExists(`Client Id`, value.data.message?.client_id)}
+                {renderRowIfExists(
+                  `Message Id`,
+                  value.data.message?.message_id,
+                )}
+                {renderRowIfExists(`Request`, value.data.database?.request)}
+                {renderRowIfExists(
+                  `Request`,
+                  value.data.database?.result?.request,
+                )}
+                {renderRowIfExists(`Request`, value.data.mixed?.request)}
+                {renderRowIfExists(`Type`, value.data.openai?.type)}
+                {renderRowIfExists(`Action`, value.data.channel?.action)}
+                {value.duration ? (
+                  <Tooltip label="Duration" offset={5} position="bottom">
+                    <Group gap={4}>
+                      <IconClockFilled size={12} />
+                      <Text size={"xs"}>
+                        {Number.parseFloat(value.duration).toFixed(2)} ms
+                      </Text>
+                    </Group>
+                  </Tooltip>
+                ) : null}
+                <Divider
+                  label={value.delta ? `+ ${value.delta?.toFixed(2)} ms` : null}
+                  mb={8}
+                  mt={4}
+                />
+              </Stack>
+            ))}
           </Stack>
-        </Paper>
-      ))}
+        </Tabs.Panel>
+
+        <Tabs.Panel pt={12} value="raw">
+          {trace.list.map((value, index, array) => (
+            <Paper>
+              <Stack gap={"md"}>
+                <Group justify={"space-between"}>
+                  <Stack>
+                    {value.attributes.start &&
+                    array[index - 1]?.attributes?.start ? (
+                      <Text c={"dimmed"} size={"xs"}>
+                        <strong>+</strong>{" "}
+                        {value.attributes.start -
+                          array[index - 1]?.attributes.start}{" "}
+                        ms
+                      </Text>
+                    ) : null}
+                    <Text c={"dimmed"} size={"xs"}>
+                      <strong>ID</strong>: {value.span_id}
+                    </Text>
+                  </Stack>
+                  <Group gap={"xs"}>
+                    {Object.keys(value.attributes)
+                      .filter((value) => filterKeys(value))
+                      .map((value) => (
+                        <Badge color={"grape"} size="xs" variant="light">
+                          {value}
+                        </Badge>
+                      ))}
+                  </Group>
+                </Group>
+                {/*@ts-ignore*/}
+                <JSONTree
+                  theme={{
+                    author: "wimer hazenberg (http://www.monokai.nl)",
+                    base00: "#272822",
+                    base01: "#383830",
+                    base02: "#49483e",
+                    base03: "#75715e",
+                    base04: "#a59f85",
+                    base05: "#f8f8f2",
+                    base06: "#f5f4f1",
+                    base07: "#f9f8f5",
+                    base08: "#f92672",
+                    base09: "#fd971f",
+                    base0A: "#f4bf75",
+                    base0B: "#a6e22e",
+                    base0C: "#a1efe4",
+                    base0D: "#66d9ef",
+                    base0E: "#ae81ff",
+                    base0F: "#cc6633",
+                    scheme: "monokai",
+                  }}
+                  data={value.attributes}
+                />
+                <Stack gap={4}>
+                  {value.attributes.duration ? (
+                    <Text c={"dimmed"} size={"xs"}>
+                      <strong>Duration</strong>:{" "}
+                      {Number.parseFloat(value.attributes.duration)} ms
+                    </Text>
+                  ) : null}
+                </Stack>
+              </Stack>
+            </Paper>
+          ))}
+        </Tabs.Panel>
+      </Tabs>
     </Stack>
   );
 };
